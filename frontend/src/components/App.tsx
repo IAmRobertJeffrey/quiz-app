7218be878c058aea30b558fa6f76a8a6b48cfa89
Rob <iamrobertjeffrey@gmail.com>
2021-12-11 00:20:47 +0000

    started working on header.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import { Typography, Container } from '@mui/material';
+import Header from './Header';
 
 function App()
 {
 	return (
 		<div className="App">
-
+			<Header />
 		</div>
 	);
 }
